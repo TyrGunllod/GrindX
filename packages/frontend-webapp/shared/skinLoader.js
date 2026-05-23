d1d22bc4a7237a9ac6cf2dbb78cc7765e8cc4d71
Alex <alex.bertoloti@gmail.com>
2026-05-22 23:53:58 -0300

    feat(skins): adicionar mais opções de fontes no editor de skins e corrigir carregamento de fontes ausentes no FONT_CDN_MAP

--- packages/frontend-webapp/shared/skinLoader.js
+++ packages/frontend-webapp/shared/skinLoader.js
@@ -43,6 +43,13 @@ const FONT_CDN_MAP = {
     Inter: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
     Roboto: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
     'Open Sans': 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap',
+    'Barlow Condensed': 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;500;600;700&display=swap',
+    'DM Sans': 'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&display=swap',
+    Lato: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;500;600;700&display=swap',
+    Montserrat: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap',
+    Poppins: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap',
+    Nunito: 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700&display=swap',
+    'Source Sans Pro': 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;500;600;700&display=swap',
 };
 
 class SkinLoader {
